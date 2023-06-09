d4f6de79d2a9109726df59d051cd45b295690320
Muhammed Afsal <muhammedafsalvr@gmail.com>
2023-06-09 17:53:33 +0530

    final commit

--- food/admin.py
+++ food/admin.py
@@ -3,7 +3,7 @@ from food.models import Food
 
 
 class FoodAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "publisher_name"]
+    list_display = ["id", "name", "publisher_name", "is_deleted"]
 
 admin.site.register(Food,FoodAdmin)
 
--- foodapp/settings.py
+++ foodapp/settings.py
@@ -109,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 REST_FRAMEWORK = {
