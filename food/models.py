05554ca6d191c9de2c32f00ea6e46bcc678c5aae
Muhammed Afsal <muhammedafsalvr@gmail.com>
2023-06-12 18:55:05 +0530

    model creation

--- food/models.py
+++ food/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Food(models.Model):
     name = models.CharField(max_length=200)
-    publisher_name = models.CharField(max_length=200)
+    publisher_name = models.CharField(max_length=250
+    )
     featured_image = models.ImageField(upload_to="foods/images")
     ingredients = models.TextField()
     description = models.TextField()
